44772f43c1d332d0bcbdd59f99fc29ab50db22fe
yuuki76 <yukimorifumi@gmail.com>
2020-08-03 20:03:37 +0900

    Fix Metadata

--- Twittersudo.js
+++ Twittersudo.js
@@ -1,9 +1,12 @@
 // ==UserScript==
-// @name     TwitterのDraft画面に出る文字をsudoの注意書きに変更するやつ
-// @version  1.0.0
-// @include  https://twitter.com/*
-// @require  http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
-// @grant    none
+// @name         TwitterのDraft画面に出る文字をsudoの注意書きに変更するやつ
+// @namespace    http://tampermonkey.net/
+// @updateURL    https://github.com/yuuki76/Twittersudo/blob/master/Twittersudo.js
+// @version      1.0.0
+// @description  名前のまんま
+// @author       Kris57 aka yuuki76
+// @match        https://twitter.com/*
+// @grant        none
 // ==/UserScript==
 (
     window.onload = function() {
