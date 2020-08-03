8b4ac02b94a6881fbb7a99ad4903da6f3e55284f
yuuki76 <yukimorifumi@gmail.com>
2020-08-03 20:06:46 +0900

    fix

--- Twittersudo.js
+++ Twittersudo.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         TwitterのDraft画面に出る文字をsudoの注意書きに変更するやつ
-// @namespace    https://greasyfork.org/ja/scripts/408117-twitter%E3%81%AEdraft%E7%94%BB%E9%9D%A2%E3%81%AB%E5%87%BA%E3%82%8B%E6%96%87%E5%AD%97%E3%82%92sudo%E3%81%AE%E6%B3%A8%E6%84%8F%E6%9B%B8%E3%81%8D%E3%81%AB%E5%A4%89%E6%9B%B4%E3%81%99%E3%82%8B%E3%82%84%E3%81%A4
+// @namespace    https://github.com/yuuki76
 // @updateURL    https://github.com/yuuki76/Twittersudo/blob/master/Twittersudo.js
 // @version      1.0.0
 // @description  名前のまんま
